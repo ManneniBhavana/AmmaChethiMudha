be8c2599757f53f27a65e8a7f0d418714c96199f
ManneniBhavana <Bhavana.Manneni001@umb.edu>
2024-04-24 01:04:50 -0400

    Files created

---
+++ src/Dashboard/Header/HeaderPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // If using React Router for navigation
+import './Header.css'; // Import the CSS file for styling
+
+const Header = ({}) => {
+    return (
+        <></>
+    );
+}
+
+export default Header;
